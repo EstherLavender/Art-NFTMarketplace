ef696fbeff3144279dfeda398211a2c67b7bb377
unknown <lavenderesther@outlook.com>
2023-06-02 12:00:15 +0300

    first commit

---
+++ src/frontend/components/Home.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import { Row, Col, Card, Button } from 'react-bootstrap'
+
+const Home = ({ marketplace, nft }) => {
+  const [loading, setLoading] = useState(true)
+  const [items, setItems] = useState([])
+  const loadMarketplaceItems = async () => {
+    // Load all unsold items
+    const itemCount = await marketplace.itemCount()
+    let items = []
+    for (let i = 1; i <= itemCount; i++) {
+      const item = await marketplace.items(i)
+      if (!item.sold) {
+        // get uri url from nft contract
+        const uri = await nft.tokenURI(item.tokenId)
+        // use uri to fetch the nft metadata stored on ipfs 
+        const response = await fetch(uri)
+        const metadata = await response.json()
+        // get total price of item (item price + fee)
+        const totalPrice = await marketplace.getTotalPrice(item.itemId)
+        // Add item to items array
+        items.push({
+          totalPrice,
+          itemId: item.itemId,
+          seller: item.seller,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image
+        })
+      }
+    }
+    setLoading(false)
+    setItems(items)
+  }
+
+  const buyMarketItem = async (item) => {
+    await (await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })).wait()
+    loadMarketplaceItems()
+  }
+
+  useEffect(() => {
+    loadMarketplaceItems()
+  }, [])
+  if (loading) return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Loading...</h2>
+
+    </main>
+  )
+  return (
+    <div className="flex justify-center">
+      <p>Artist; Kisato : I am a versatile artist who specializes in painting, charcoal work, and abstract art. Through my artistic endeavors, I strive to express my unique vision and emotions, capturing the essence of the subject matter in a variety of mediums.
+In my paintings, I explore different styles and subjects, ranging from portraits to depict emotions and abstract compositions. I enjoy experimenting with various techniques, brushstrokes, and color palettes to bring my ideas to life on the canvas. Each stroke is an opportunity for me to convey the depth of my artistic expression and connect with viewers on an emotional level.</p>
+<p>Working with charcoal allows me to create dramatic, textured pieces that emphasize light and shadow. I find great joy in the process of smudging and blending the charcoal to achieve intricate details and captivating contrasts.
+Overall, my artistic practice is a reflection of my passion for self-expression, experimentation, and pushing the boundaries of traditional art forms. I am constantly exploring new techniques and mediums to expand my creative horizons and deliver captivating visual experiences to art enthusiasts and collectors.</p>
+      {items.length > 0 ?
+        <div className="px-5 container">
+          <Row xs={1} md={2} lg={4} className="g-4 py-5">
+            {items.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Body color="secondary">
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <div className='d-grid'>
+                      <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
+                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+                      </Button>
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        : (
+          <main style={{ padding: "1rem 0" }}>
+            <h2>No listed assets</h2>
+          </main>
+        )}
+    </div>
+  );
+}
+export default Home
